aa3c9faf7ca2192bee00a8ca191302aadbedb3eb
ahmad <ahmadnadeem5961929>
2019-11-16 11:44:48 -0800

    working on splash screen

--- app/src/main/java/lcwu/fyp/gohytch/SplashActivity.java
+++ app/src/main/java/lcwu/fyp/gohytch/SplashActivity.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.rbddevs.splashy.Splashy;
+
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        setSplashy();
     }
+   void setSplashy() {
+       new Splashy(this)         // For JAVA : new Splashy(this)
+                .setLogo(R.drawable.logo)
+                .setTitle("Splashy")
+                .setTitleColor("#FFFFFF")
+                .setSubTitle("Splash screen made easy")
+                .setProgressColor(R.color.white)
+              //  .setBackgroundResource("#000000")
+                .setFullScreen(true)
+                .setTime(5000)
+               .showProgress(true)
+               .setAnimation(Splashy.Animation.SLIDE_IN_TOP_BOTTOM, 1000)
+                .show();
+    }
+
 }
