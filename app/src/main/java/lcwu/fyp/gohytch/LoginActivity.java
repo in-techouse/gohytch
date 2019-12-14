0ecd0d5d05ac2bb14633615070bec3199d25b9d2
Muhammad Taimoor Sultani <team.dev.man@gmail.com>
2019-12-14 13:02:34 +0500

    Auth error resolved

--- app/src/main/java/lcwu/fyp/gohytch/LoginActivity.java
+++ app/src/main/java/lcwu/fyp/gohytch/LoginActivity.java
@@ -126,13 +126,37 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                                 public void onFailure(@NonNull Exception e) {
                                     LoginProgress.setVisibility(View.GONE);
                                     btnLogin.setVisibility(View.VISIBLE);
+                                    new FancyBottomSheetDialog.Builder(LoginActivity.this)
+                                            .setTitle("ERROR!")
+                                            .setMessage(e.getMessage())
+                                            .setBackgroundColor(Color.parseColor("#F43636")) //don't use R.color.somecolor
+                                            .setIcon(R.drawable.ic_action_error,true)
+                                            .isCancellable(false)
+                                            .OnNegativeClicked(new FancyBottomSheetDialog.FancyBottomSheetDialogListener() {
+                                                @Override
+                                                public void OnClick() {
 
+                                                }
+                                            })
+                                            .OnPositiveClicked(new FancyBottomSheetDialog.FancyBottomSheetDialogListener() {
+                                                @Override
+                                                public void OnClick() {
+
+                                                }
+                                            })
+                                            .setNegativeBtnText("Cancel")
+                                            .setPositiveBtnText("Ok")
+                                            .setPositiveBtnBackground(Color.parseColor("#F43636"))//don't use R.color.somecolor
+                                            .setNegativeBtnBackground(Color.WHITE)//don't use R.color.somecolor
+                                            .build();
                                 }
                             });
                         }
 
                         @Override
                         public void onVerificationFailed(@NonNull FirebaseException e) {
+                            LoginProgress.setVisibility(View.GONE);
+                            btnLogin.setVisibility(View.VISIBLE);
                             new FancyBottomSheetDialog.Builder(LoginActivity.this)
                                     .setTitle("ERROR!")
                                     .setMessage(e.getMessage())
