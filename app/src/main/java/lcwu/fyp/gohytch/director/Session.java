44f222e3f62e5ccb0583b5466a6dc7eca9b83de6
Aqsa <aqsachughtai2@gmail.com>
2019-12-22 12:06:11 -0800

    Session

---
+++ app/src/main/java/lcwu/fyp/gohytch/director/Session.java
@@ -0,0 +1,46 @@
+package lcwu.fyp.gohytch.director;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.google.gson.Gson;
+
+import lcwu.fyp.gohytch.model.User;
+
+public class Session {
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+    private Context context;
+
+    public Session(Context c){
+        context = c;
+        preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        editor = preferences.edit();
+    }
+
+    public void setSession(User user){
+
+        Gson gson = new Gson();
+        String value = gson.toJson(user);
+        editor.putString("user", value);
+        editor.apply();
+    }
+
+    public User getSession(){
+        String str = preferences.getString("user", "*");
+        if(str != null && str.equals("*"))
+            return null;
+        Gson gson = new Gson();
+        User u = gson.fromJson(str, User.class);
+        return  u;
+    }
+
+    public void destroySession(){
+        editor.remove("user");
+        editor.apply();
+    }
+
+
+
+}
