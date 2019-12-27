7d0df3fef0225bd39ef18bf989e212d8364ba741
Aqsa <aqsachughtai2@gmail.com>
2019-12-27 13:02:13 -0800

    dashboard activity

--- app/src/main/java/lcwu/fyp/gohytch/Activities/UserProfileActivity.java
+++ app/src/main/java/lcwu/fyp/gohytch/Activities/UserProfileActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -16,11 +18,13 @@ import android.widget.ProgressBar;
 
 import lcwu.fyp.gohytch.R;
 import lcwu.fyp.gohytch.director.Helpers;
+import lcwu.fyp.gohytch.director.Session;
+import lcwu.fyp.gohytch.model.User;
 
 public class UserProfileActivity extends AppCompatActivity implements View.OnClickListener{
     Button btnSave;
     EditText edtphonenumber,edtName,edtEmail;
-    String strphonenumber,strName,strEmail;
+    String strPhonenumber,strName,strEmail;
     ProgressBar SaveProgress;
     String verificationId;
     Helpers helpers;
@@ -30,6 +34,7 @@ public class UserProfileActivity extends AppCompatActivity implements View.OnCli
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
         btnSave=findViewById(R.id.btnSave);
+        edtphonenumber=findViewById(R.id.PhoneNumber);
         edtName=findViewById(R.id.Name);
         edtEmail=findViewById(R.id.Email);
         SaveProgress=findViewById(R.id.SaveProgress);
@@ -58,37 +63,51 @@ public class UserProfileActivity extends AppCompatActivity implements View.OnCli
             case R.id.btnSave: {
                 boolean isConn = helpers.isConnected(UserProfileActivity.this);
                 if (!isConn) {
+
                     helpers.showError(UserProfileActivity.this, "ERROR!", "No Internet Connection. Please check your Internet Connection.");
                     return;
                 }
-                String strName=edtName.getText().toString();
-                String strEmail = edtEmail.getText().toString();
-                String strPhonenumber = edtphonenumber.getText().toString();
+                boolean flag=isValid();
+                if(!flag){
+                    User u=new User();
+                    Session session=new Session(UserProfileActivity.this);
 
-                if (strName.length() < 3) {
-                    edtName.setError("Enter a valid name");
-                } else {
-                    edtName.setError(null);
-                }
-
-                if (strPhonenumber.length() != 11) {
-                    edtphonenumber.setError("Enter a valid number");
-
-                } else {
-                    edtphonenumber.setError(null);
-                }
-                if (strEmail.length() < 6 || !Patterns.EMAIL_ADDRESS.matcher(strEmail).matches()) {
-                    edtEmail.setError("Enter a valid email");
-                } else {
-                    edtEmail.setError(null);
+                    DatabaseReference reference= FirebaseDatabase.getInstance().getReference();
 
                 }
 
-
-
-                }
             }
         }
-
     }
 
+    private boolean isValid() {
+        boolean flag = true;
+        strName=edtName.getText().toString();
+        strEmail = edtEmail.getText().toString();
+        strPhonenumber = edtphonenumber.getText().toString();
+
+        if (strName.length() < 3) {
+            edtName.setError("Enter a valid name");
+            flag=false;
+        } else {
+            edtName.setError(null);
+        }
+
+        if (strPhonenumber.length() != 11) {
+            edtphonenumber.setError("Enter a valid number");
+            flag=false;
+
+        } else {
+            edtphonenumber.setError(null);
+        }
+        if (strEmail.length() < 6 || !Patterns.EMAIL_ADDRESS.matcher(strEmail).matches()) {
+            edtEmail.setError("Enter a valid email");
+            flag=false;
+        } else {
+            edtEmail.setError(null);
+
+        }
+        return flag;
+    }
+
+}
