2a461dbbb742cc0afdbc21f6e2b3e513a182deb7
Aqsa <aqsachughtai2@gmail.com>
2019-12-29 12:35:50 -0800

    working on maps

--- app/src/main/java/lcwu/fyp/gohytch/Activities/DashboardActivity.java
+++ app/src/main/java/lcwu/fyp/gohytch/Activities/DashboardActivity.java
@@ -1,14 +1,26 @@
 package lcwu.fyp.gohytch.Activities;
 
+import android.app.Notification;
 import android.os.Bundle;
 
+import com.google.android.gms.common.internal.Constants;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.provider.SyncStateContract;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.ThemedSpinnerAdapter;
 import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
@@ -34,8 +46,12 @@ import lcwu.fyp.gohytch.director.Session;
 import lcwu.fyp.gohytch.model.User;
 
 public class DashboardActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener{
+    private MapView map;
     private Helpers helpers;
     private User user;
+    private GoogleMap googleMap;
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
     private Session session;
 
     private CircleImageView Profile_Image;
@@ -45,33 +61,58 @@ public class DashboardActivity extends AppCompatActivity  implements NavigationV
 
 
 
-    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        helpers=new Helpers();
-        session=new Session(DashboardActivity.this);
-        user=session.getSession();
+        helpers = new Helpers();
+        session = new Session(DashboardActivity.this);
+        user = session.getSession();
 
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        View header= navigationView.getHeaderView(0);
+        navigationView = findViewById(R.id.nav_view);
+        View header = navigationView.getHeaderView(0);
         navigationView.setNavigationItemSelectedListener(this);
-        profile_Phone=header.findViewById(R.id.profile_Phone);
-        profile_Name=header.findViewById(R.id.profile_Name);
-        profile_Email=header.findViewById(R.id.profile_Email);
-        Profile_Image=header.findViewById(R.id.profile_image);
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toogle);
+        toogle.syncState();
+        profile_Phone = header.findViewById(R.id.profile_Phone);
+        profile_Name = header.findViewById(R.id.profile_Name);
+        profile_Email = header.findViewById(R.id.profile_Email);
+        Profile_Image = header.findViewById(R.id.profile_image);
 
         profile_Name.setText(user.getName());
         profile_Email.setText(user.getEmail());
         profile_Phone.setText(user.getPhoneNumber());
+        map = findViewById(R.id.map);
+        map.onCreate(savedInstanceState);
+        try {
+            MapsInitializer.initialize(DashboardActivity.this);
+            Log.e("Maps", "Try Block");
+            map.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap gM) {
+                    Log.e("Maps", "Call back received");
+                    googleMap = gM;
+                    LatLng defaultPosition = new LatLng(31.5204, 74.3487);
+                    CameraPosition cameraPosition = new CameraPosition.Builder().target(defaultPosition).zoom(12).build();
+                    googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+
+                }
+            });
+        }
+        catch (Exception e){
+            Log.e("Maps", "Catch Block");
+            helpers.showError(DashboardActivity.this, "Error", "Something went wrong.Try again");
+        }
     }
+
 
 
 
@@ -95,6 +136,33 @@ public class DashboardActivity extends AppCompatActivity  implements NavigationV
         }
         drawer.closeDrawer(GravityCompat.START);
         return false;
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        map.onResume();
+
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        map.onLowMemory();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        map.onDestroy();
 
     }
 }
