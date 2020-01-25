be26f25a4fd702057d987614f930a732c0ef77ac
ahmad <ahmadnadeem5961929>
2020-01-25 13:03:01 +0500

    driver

--- app/src/main/java/lcwu/fyp/gohytch/activities/CreateDriver.java
+++ app/src/main/java/lcwu/fyp/gohytch/activities/CreateDriver.java
@@ -3,14 +3,34 @@ package lcwu.fyp.gohytch.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import lcwu.fyp.gohytch.R;
 
-public class CreateDriver extends AppCompatActivity {
+public class CreateDriver extends AppCompatActivity implements View.OnClickListener {
+    Button btnSave;
+    EditText edtLicenseNumber,edtPastExperience,edtExpertise;
+    ProgressBar SaveProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_driver);
+
+        btnSave=findViewById(R.id.btnSave);
+        edtLicenseNumber=findViewById(R.id.LicenseNumber);
+        edtPastExperience=findViewById(R.id.PastExperience);
+        edtExpertise=findViewById(R.id.Expertise);
+        SaveProgress=findViewById(R.id.SaveProgress);
+        btnSave.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
