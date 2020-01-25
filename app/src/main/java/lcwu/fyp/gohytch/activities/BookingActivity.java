6a28d4e668b75b0ca826858ee118ae8e0b36e574
ahmad <ahmadnadeem5961929>
2020-01-25 11:53:07 +0500

    driver

--- app/src/main/java/lcwu/fyp/gohytch/activities/BookingActivity.java
+++ app/src/main/java/lcwu/fyp/gohytch/activities/BookingActivity.java
@@ -1,24 +1,40 @@
 package lcwu.fyp.gohytch.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import lcwu.fyp.gohytch.R;
 
 public class BookingActivity extends AppCompatActivity {
+    private LinearLayout loading;
+    private TextView noRecord;
+    private RecyclerView notification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
+
+         loading=findViewById(R.id.loadig);
+         noRecord=findViewById(R.id.noRecord);
+         notification=findViewById(R.id.notification);
+
+
+
+
     }
 
     @Override
     public void onBackPressed() {
         finish();
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
