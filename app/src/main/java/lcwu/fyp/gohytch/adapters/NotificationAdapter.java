c1a8dbfa421356ef7148a58022ffcfce6dca9160
Aqsa <aqsachughtai2@gmail.com>
2020-01-31 12:55:34 -0800

    item_notification

--- app/src/main/java/lcwu/fyp/gohytch/adapters/NotificationAdapter.java
+++ app/src/main/java/lcwu/fyp/gohytch/adapters/NotificationAdapter.java
@@ -1,4 +1,52 @@
 package lcwu.fyp.gohytch.adapters;
 
-public class NotificationAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lcwu.fyp.gohytch.R;
+import lcwu.fyp.gohytch.model.Notification;
+
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.NotificationHolder> {
+    private List<Notification>data;
+
+    public NotificationAdapter() {
+        data=new ArrayList<>();
+    }
+
+    public void setData(List<Notification> data) {
+        this.data = data;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public NotificationHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_notification,parent,false);
+        return new NotificationHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotificationHolder holder, int position) {
+        final Notification n=data.get(position);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    class NotificationHolder extends RecyclerView.ViewHolder {
+
+        public NotificationHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
