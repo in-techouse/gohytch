2d450c145f15f37189c6e4fa7b1501a48a9fe371
ahmad <ahmadnadeem5961929>
2020-02-01 13:01:58 +0500

    image slider

--- app/src/main/java/lcwu/fyp/gohytch/activities/CreateDriver.java
+++ app/src/main/java/lcwu/fyp/gohytch/activities/CreateDriver.java
@@ -2,12 +2,24 @@ package lcwu.fyp.gohytch.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.smarteist.autoimageslider.IndicatorAnimations;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+import com.smarteist.autoimageslider.SliderViewAdapter;
 
 import lcwu.fyp.gohytch.R;
 import lcwu.fyp.gohytch.director.Helpers;
@@ -26,13 +38,31 @@ public class CreateDriver extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_driver);
 
-        btnSave=findViewById(R.id.btnSave);
-        edtLicenseNumber=findViewById(R.id.LicenseNumber);
-        edtExpertise=findViewById(R.id.Expertise);
-        SaveProgress=findViewById(R.id.SaveProgress);
+
+        btnSave = findViewById(R.id.btnSave);
+        edtLicenseNumber = findViewById(R.id.LicenseNumber);
+        edtExpertise = findViewById(R.id.Expertise);
+        SaveProgress = findViewById(R.id.SaveProgress);
         btnSave.setOnClickListener(this);
-        helpers=new Helpers();
+        helpers = new Helpers();
+
+
+        SliderView sliderView = findViewById(R.id.imageSlider);
+
+        SliderAdapter adapter = new SliderAdapter(CreateDriver.this);
+
+        sliderView.setSliderAdapter(adapter);
+
+        sliderView.setIndicatorAnimation(IndicatorAnimations.WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
+        sliderView.setIndicatorSelectedColor(Color.WHITE);
+        sliderView.setIndicatorUnselectedColor(Color.GRAY);
+        sliderView.setScrollTimeInSec(4); //set scroll delay in seconds :
+        sliderView.startAutoCycle();
+
     }
+
 
     @Override
     public void onClick(View v) {
@@ -67,5 +97,65 @@ public class CreateDriver extends AppCompatActivity implements View.OnClickListe
             edtLicenseNumber.setError(null);
         }
         return flag;
+    }
+    public class SliderAdapter extends SliderViewAdapter<SliderAdapter.SliderAdapterVH> {
+
+        private Context context;
+
+        public SliderAdapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public SliderAdapterVH onCreateViewHolder(ViewGroup parent) {
+            View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.image_slider_layout_item, null);
+            return new SliderAdapterVH(inflate);
+        }
+
+        @Override
+        public void onBindViewHolder(SliderAdapterVH viewHolder, int position) {
+
+            switch (position) {
+                case 0:
+                    Glide.with(viewHolder.itemView)
+                            .load("https://images.pexels.com/photos/218983/pexels-photo-218983.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+                            .into(viewHolder.imageViewBackground);
+                    break;
+                case 1:
+                    Glide.with(viewHolder.itemView)
+                            .load("https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260")
+                            .into(viewHolder.imageViewBackground);
+                    break;
+                case 2:
+                    Glide.with(viewHolder.itemView)
+                            .load("https://images.pexels.com/photos/929778/pexels-photo-929778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+                            .into(viewHolder.imageViewBackground);
+                    break;
+                default:
+                    Glide.with(viewHolder.itemView)
+                            .load("https://images.pexels.com/photos/218983/pexels-photo-218983.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260")
+                            .into(viewHolder.imageViewBackground);
+                    break;
+
+            }
+
+        }
+
+        @Override
+        public int getCount() {
+            //slider view count could be dynamic size
+            return 4;
+        }
+
+        class SliderAdapterVH extends SliderViewAdapter.ViewHolder {
+            View itemView;
+            ImageView imageViewBackground;
+
+            public SliderAdapterVH(View itemView) {
+                super(itemView);
+                imageViewBackground = itemView.findViewById(R.id.iv_auto_image_slider);
+                this.itemView = itemView;
+            }
+        }
     }
 }
