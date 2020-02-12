a564867787671d1ed89dc70b4f78cd995bcc02f7
zeeshan ashraf <zeeshanashraf9t@gmail.com>
2020-02-12 16:22:03 +0500

    Profile Update Enabled

--- app/src/main/java/lcwu/fyp/gohytch/activities/EditUserProfile.java
+++ app/src/main/java/lcwu/fyp/gohytch/activities/EditUserProfile.java
@@ -1,20 +1,66 @@
 package lcwu.fyp.gohytch.activities;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.asksira.bsimagepicker.BSImagePicker;
+import com.asksira.bsimagepicker.Utils;
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
+import android.util.Log;
+import android.util.Patterns;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import java.util.Calendar;
 
 import lcwu.fyp.gohytch.R;
+import lcwu.fyp.gohytch.director.Helpers;
+import lcwu.fyp.gohytch.director.Session;
+import lcwu.fyp.gohytch.model.User;
 
-public class EditUserProfile extends AppCompatActivity {
+public class EditUserProfile extends AppCompatActivity implements View.OnClickListener , BSImagePicker.OnSingleImageSelectedListener , BSImagePicker.ImageLoaderDelegate {
 
+    private final String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+    };
+    private EditText profileName , profileEmail , profilePhoneNumber;
+    private Button updateBtn;
+    String strPhonenumber,strName,strEmail;
+    ProgressBar SaveProgress;
+    Helpers helpers;
+    ImageView image;
+    boolean isImage = false;
+    Uri imageUri;
+    User user;
+    Session session;
+    DatabaseReference updateReference = FirebaseDatabase.getInstance().getReference().child("Users");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +68,28 @@ public class EditUserProfile extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.nav_gallery);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        FloatingActionButton fab = findViewById(R.id.profile_nav_gallery);
+        profileName = findViewById(R.id.profileName);
+        profileEmail = findViewById(R.id.profileEmail);
+        profilePhoneNumber = findViewById(R.id.profilePhoneNumber);
+        updateBtn = findViewById(R.id.btnUpdate);
+        session = new Session(EditUserProfile.this);
+        helpers = new Helpers();
+        user =  session.getSession();
+        fab.setOnClickListener(this);
+        updateBtn.setOnClickListener(this);
+        SaveProgress=findViewById(R.id.profileSaveProgress);
+
+        image = findViewById(R.id.profileImage);
+        image.setImageDrawable(getResources().getDrawable(R.drawable.profile));
+        AppBarLayout app_bar = findViewById(R.id.app_bar);
+        app_bar.setExpanded(true, true);
+
+        profileName.setText(user.getName());
+        profilePhoneNumber.setText(user.getPhoneNumber());
+        profileEmail.setText(user.getEmail());
+
+
     }
 
     @Override
@@ -46,5 +106,178 @@ public class EditUserProfile extends AppCompatActivity {
             }
         }
         return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id=view.getId();
+        switch (id){
+            case R.id.btnUpdate: {
+                Log.e("User", "Button Clicked");
+                boolean isConn = helpers.isConnected(EditUserProfile.this);
+                if (!isConn) {
+                    helpers.showError(EditUserProfile.this, "ERROR!", "No Internet Connection. Please check your Internet Connection.");
+                    return;
+                }
+                boolean flag=isValid();
+                if(flag){
+                    SaveProgress.setVisibility(View.VISIBLE);
+                    updateBtn.setVisibility(View.GONE);
+                    user.setEmail(strEmail);
+                    user.setName(strName);
+                    Log.e("Login", "Id: " + user.getId());
+                    Log.e("Login", "Email: " + user.getEmail());
+                    Log.e("Login", "Name: " + user.getName());
+                    Log.e("Login", "Phone Number: " + user.getPhoneNumber());
+                    if(isImage){
+                        uploadImage();
+                    }
+                    else{
+                        user.setImage("");
+                        saveUser();
+                    }
+
+                }
+                break;
+            }
+            case R.id.profile_nav_gallery:{
+                boolean flag = hasPermissions(EditUserProfile.this, PERMISSIONS);
+                if(!flag){
+                    ActivityCompat.requestPermissions(EditUserProfile.this, PERMISSIONS, 1);
+                }
+                else{
+                    openGallery();
+                }
+                break;
+            }
+        }
+
+    }
+
+    private void uploadImage(){
+        final StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("Users").child(user.getId());
+//        Uri selectedMediaUri = Uri.parse(imagePath.toString());
+        Calendar calendar = Calendar.getInstance();
+        storageReference.child(calendar.getTimeInMillis()+"").putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        taskSnapshot.getMetadata().getReference().getDownloadUrl()
+                                .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        user.setImage(uri.toString());
+                                        saveUser();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.e("Login", "Upload Image Url: " + e.getMessage());
+                                        SaveProgress.setVisibility(View.GONE);
+                                        updateBtn.setVisibility(View.VISIBLE);
+                                        helpers.showError(EditUserProfile.this, "ERROR!", "Something went wrong.\n Please try again later.");
+                                    }
+                                });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("Login", "Upload Image Url: " + e.getMessage());
+                        SaveProgress.setVisibility(View.GONE);
+                        updateBtn.setVisibility(View.VISIBLE);
+                        helpers.showError(EditUserProfile.this, "ERROR!", "Something went wrong.\n Please try again later.");
+                    }
+                });
+    }
+
+    private void saveUser(){
+        Log.e("Login", "Id: " + user.getId());
+        Log.e("Login", "Email: " + user.getEmail());
+        Log.e("Login", "Name: " + user.getName());
+        Log.e("Login", "Phone Number: " + user.getPhoneNumber());
+        Log.e("Login", "Image: " + user.getImage());
+        user.setLat(0);
+        user.setLng(0);
+        updateReference.child(user.getId()).setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                SaveProgress.setVisibility(View.GONE);
+                updateBtn.setVisibility(View.VISIBLE);
+                session.setSession(user);
+                Intent it=new Intent(EditUserProfile.this,DashboardActivity.class);
+                it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(it);
+                finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                SaveProgress.setVisibility(View.GONE);
+                updateBtn.setVisibility(View.VISIBLE);
+                helpers.showError(EditUserProfile.this, "ERROR", "Something went wrong.\nPlease try again later.");
+            }
+        });
+    }
+
+    private boolean hasPermissions(Context c, String... permission){
+        for(String p : permission){
+            if(ActivityCompat.checkSelfPermission(c, p) != PackageManager.PERMISSION_GRANTED){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void openGallery(){
+        BSImagePicker singleSelectionPicker = new BSImagePicker.Builder("lcwu.fyp.gohytch.fileprovider")
+                .setMaximumDisplayingImages(24) //Default: Integer.MAX_VALUE. Don't worry about performance :)
+                .setSpanCount(3) //Default: 3. This is the number of columns
+                .setGridSpacing(Utils.dp2px(2)) //Default: 2dp. Remember to pass in a value in pixel.
+                .setPeekHeight(Utils.dp2px(360)) //Default: 360dp. This is the initial height of the dialog.
+                .hideCameraTile() //Default: show. Set this if you don't want user to take photo.
+                .hideGalleryTile() //Default: show. Set this if you don't want to further let user select from a gallery app. In such case, I suggest you to set maximum displaying images to Integer.MAX_VALUE.
+                .setTag("A request ID") //Default: null. Set this if you need to identify which picker is calling back your fragment / activity.
+                .build();
+        singleSelectionPicker.show(getSupportFragmentManager(), "picker");
+    }
+
+
+
+    private boolean isValid() {
+        boolean flag = true;
+        strName=profileName.getText().toString();
+        strEmail = profileEmail.getText().toString();
+        Log.e("User", "Email: " + strEmail);
+        if (strName.length() < 3) {
+            profileName.setError("Enter a valid name");
+            flag=false;
+        } else {
+            profileName.setError(null);
+        }
+
+        if (strEmail.length() < 6 || !Patterns.EMAIL_ADDRESS.matcher(strEmail).matches()) {
+            Log.e("User", "If Email: " + strEmail);
+            profileEmail.setError("Enter a valid email");
+            flag=false;
+        } else {
+            Log.e("User", "Else Email: " + strEmail);
+            profileEmail.setError(null);
+        }
+        return flag;
+    }
+
+    @Override
+    public void onSingleImageSelected(Uri uri, String tag) {
+        isImage = true;
+        imageUri = uri;
+        Glide.with(EditUserProfile.this).load(imageUri).into(image);
+
+    }
+
+    @Override
+    public void loadImage(Uri imageUri, ImageView ivImage) {
+
     }
 }
